f56b4472e14a8c9469dfcc8f7bd9d4e08a45fc6c
lennvdh <lenn.vanderheijden@gmail.com>
2020-06-25 11:59:59 +0200

    ()

--- js.js
+++ js.js
@@ -5,9 +5,14 @@ const rock = document.getElementById('rock');
 const scissor = document.getElementById('scissor');
 const whoWon = document.getElementById('whoWon');
 const start = document.getElementById('startbutton');
+const scoreBoard = document.getElementsByClassName('scoreboard');
+const userScore = document.getElementById('userscore');
+const computerScore = document.getElementById('computerscore');
+let userPoints = 0;
+let computerPoints = 0;
 
 function startgame(){
-    mainDiv.style.display = 'contents'
+    mainDiv.style.display = 'contents';
     start.style.display = 'none';
 }
 function computerChoice(){
@@ -20,15 +25,25 @@ function game(userchoice){
     if (computer === 1){
         console.log('draw');
         whoWon.style.color = "blue";
+        userScore.style.color = "blue";
+        computerScore.style.color = "blue";
         whoWon.innerHTML = 'draw';
     }else if(computer === 2){
         console.log('you win');
         whoWon.style.color = "green";
+        userScore.style.color = "green";
+        computerScore.style.color = "green";
+        userPoints++;
         whoWon.innerHTML = 'You Won';
+        userScore.innerHTML = userPoints;
     }else{
         console.log('you lose');
         whoWon.style.color = "red"
+        userScore.style.color = "red";
+        computerScore.style.color = "red";
+        computerPoints++;
         whoWon.innerHTML = 'You Lose';
+        computerScore.innerHTML = computerPoints;
     }
 }
 function main(){
@@ -44,13 +59,3 @@ function main(){
 }
 
 main();
-
-
-
-
-
-
-
-
-
-
